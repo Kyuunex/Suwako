c563e8295c35e63af053c1b59bbb3933411c1d28
Kyuunex <kyuunex@protonmail.ch>
2020-01-25 16:14:58 +0400

    fix adding admins on first run

--- suwako.py
+++ suwako.py
@@ -62,8 +62,13 @@ class Suwako(commands.Bot):
         print("------")
         if not db.query("SELECT * FROM admins"):
             app_info = await self.application_info()
-            db.query(["INSERT INTO admins VALUES (?, ?)", [str(app_info.owner.id), "1"]])
-            print(f"Added {app_info.owner.name} to admin list")
+            if app_info.team:
+                for team_member in app_info.team.members:
+                    db.query(["INSERT INTO admins VALUES (?, ?)", [str(team_member.id), "1"]])
+                    print(f"Added {team_member.name} to admin list")
+            else:
+                db.query(["INSERT INTO admins VALUES (?, ?)", [str(app_info.owner.id), "1"]])
+                print(f"Added {app_info.owner.name} to admin list")
 
 
 client = Suwako(command_prefix="-")
