8ac04d7698cba0e935cec3776416761e3df7fc11
Kyuunex <kyuunex@protonmail.ch>
2020-02-18 19:17:11 +0400

    port modules to aiosqlite

--- modules/first_run.py
+++ modules/first_run.py
@@ -1,33 +1,41 @@
-from modules import db
+import sqlite3
 from modules.connections import database_file as database_file
 import os
 
 
 async def add_admins(self):
-    if not db.query("SELECT * FROM admins"):
+    async with await self.db.execute("SELECT * FROM admins") as cursor:
+        admin_list = await cursor.fetchall()
+
+    if not admin_list:
         app_info = await self.application_info()
         if app_info.team:
             for team_member in app_info.team.members:
-                db.query(["INSERT INTO admins VALUES (?, ?)", [str(team_member.id), "1"]])
+                await self.db.execute("INSERT INTO admins VALUES (?, ?)", [str(team_member.id), "1"])
                 print(f"Added {team_member.name} to admin list")
         else:
-            db.query(["INSERT INTO admins VALUES (?, ?)", [str(app_info.owner.id), "1"]])
+            await self.db.execute("INSERT INTO admins VALUES (?, ?)", [str(app_info.owner.id), "1"])
             print(f"Added {app_info.owner.name} to admin list")
+        await self.db.commit()
 
 
 def create_tables():
     if not os.path.exists(database_file):
-        db.query("CREATE TABLE users "
-                 "(user_id, osu_id, osu_username, osu_join_date, pp, country, ranked_maps_amount, no_sync)")
-        db.query("CREATE TABLE config (setting, parent, value, flag)")
+        conn = sqlite3.connect(database_file)
+        c = conn.cursor()
+        c.execute("CREATE TABLE config (setting, parent, value, flag)")
+        c.execute("CREATE TABLE admins (user_id, permissions)")
+        c.execute("CREATE TABLE users "
+                  "(user_id, osu_id, osu_username, osu_join_date, pp, country, ranked_maps_amount, no_sync)")
 
-        db.query("CREATE TABLE channels (setting, guild_id, channel_id)")
-        db.query("CREATE TABLE roles (setting, guild_id, role_id)")
-        db.query("CREATE TABLE country_roles (country, guild_id, role_id)")
-        db.query("CREATE TABLE pp_roles (pp, guild_id, role_id)")
+        c.execute("CREATE TABLE channels (setting, guild_id, channel_id)")
+        c.execute("CREATE TABLE roles (setting, guild_id, role_id)")
+        c.execute("CREATE TABLE country_roles (country, guild_id, role_id)")
+        c.execute("CREATE TABLE pp_roles (pp, guild_id, role_id)")
 
-        db.query("CREATE TABLE admins (user_id, permissions)")
-        db.query("CREATE TABLE restricted_users (guild_id, osu_id)")
-        db.query("CREATE TABLE member_goodbye_messages (message)")
-        db.query(["INSERT INTO member_goodbye_messages VALUES (?)", ["%s double tapped on hidamari no uta"]])
-        db.query(["INSERT INTO member_goodbye_messages VALUES (?)", ["%s missed the last note"]])
+        c.execute("CREATE TABLE restricted_users (guild_id, osu_id)")
+        c.execute("CREATE TABLE member_goodbye_messages (message)")
+        c.execute("INSERT INTO member_goodbye_messages VALUES (?)", ["%s double tapped on hidamari no uta"])
+        c.execute("INSERT INTO member_goodbye_messages VALUES (?)", ["%s missed the last note"])
+        conn.commit()
+        conn.close()
