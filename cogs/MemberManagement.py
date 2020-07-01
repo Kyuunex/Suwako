cb2dc33eadf73a29447579092976354d061dacb6
Kyuunex <kyuunex@protonmail.ch>
2020-07-01 12:10:01 +0400

    grab MemberManagement.py changes from seija

--- cogs/MemberManagement.py
+++ cogs/MemberManagement.py
@@ -1,5 +1,7 @@
+import discord
 from discord.ext import commands
 from modules import permissions
+from modules import wrappers
 import osuembed
 
 
@@ -7,47 +9,88 @@ class MemberManagement(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="get_members_not_in_db", brief="Get a list of users who are not in db", description="")
+    @commands.command(name="get_members_not_in_db", brief="Get a list of users who are not in db")
     @commands.check(permissions.is_owner)
+    @commands.check(permissions.is_not_ignored)
     @commands.guild_only()
     async def get_members_not_in_db(self, ctx):
-        for member in ctx.guild.members:
-            if not member.bot:
+        """
+        This command will return a list of members who are not in the bot's `users` table.
+        """
+
+        async with ctx.channel.typing():
+            buffer = ""
+            for member in ctx.guild.members:
+                if member.bot:
+                    continue
+
                 async with self.bot.db.execute("SELECT osu_id FROM users WHERE user_id = ?",
                                                [str(member.id)]) as cursor:
                     in_db_check = await cursor.fetchall()
-                if not in_db_check:
-                    await ctx.send(member.mention)
+                if in_db_check:
+                    continue
 
-    @commands.command(name="get_roleless_members", brief="Get a list of members without a role", description="")
+                buffer += f"{member.mention}\n"
+
+            embed = discord.Embed(color=0xbd3661)
+            embed.set_author(name="Server Members who are not in the database")
+        await wrappers.send_large_embed(ctx.channel, embed, buffer)
+
+    @commands.command(name="get_roleless_members", brief="Get a list of members without a role")
     @commands.check(permissions.is_owner)
+    @commands.check(permissions.is_not_ignored)
     @commands.guild_only()
-    async def get_roleless_members(self, ctx, lookup_in_db: str = None):
-        for member in ctx.guild.members:
-            if len(member.roles) < 2:
-                await ctx.send(member.mention)
-                if lookup_in_db:
-                    async with self.bot.db.execute("SELECT osu_id FROM users WHERE user_id = ?",
-                                                   [str(member.id)]) as cursor:
-                        query = await cursor.fetchall()
-                    if query:
-                        await ctx.send("person above is in my database "
-                                       f"and linked to <https://osu.ppy.sh/users/{query[0][0]}>")
+    async def get_roleless_members(self, ctx, lookup_in_db=""):
+        """
+        This command will return a list of members who do not have any roles.
+        :arg lookup_in_db: if this is not null, the bot will also see if the member is in the db
+        """
 
-    @commands.command(name="get_member_osu_profile",
-                      brief="Check which osu account is a discord account linked to", description="")
+        async with ctx.channel.typing():
+            buffer = ""
+            for member in ctx.guild.members:
+                if len(member.roles) > 1:
+                    # actually every member has @everyone role,
+                    # so, if a user has 2 roles, it means he has 1 assigned role
+                    continue
+
+                buffer += f"{member.mention}\n"
+
+                if not lookup_in_db:
+                    continue
+
+                async with self.bot.db.execute("SELECT osu_id FROM users WHERE user_id = ?",
+                                               [str(member.id)]) as cursor:
+                    query = await cursor.fetchone()
+                if not query:
+                    continue
+
+                buffer += f"    ^ <https://osu.ppy.sh/users/{query[0]}>"
+            embed = discord.Embed(color=0xbd3661)
+            embed.set_author(name="Server Members who do not have a role")
+        await wrappers.send_large_embed(ctx.channel, embed, buffer)
+
+    @commands.command(name="get_member_osu_profile", brief="Check which osu account is a discord account linked to")
     @commands.check(permissions.is_admin)
+    @commands.check(permissions.is_not_ignored)
     @commands.guild_only()
     async def get_member_osu_profile(self, ctx, *, user_id):
+        """
+        Return what osu account is a discord account linked to
+        """
+
         async with self.bot.db.execute("SELECT osu_id FROM users WHERE user_id = ?", [str(user_id)]) as cursor:
-            osu_id = await cursor.fetchall()
-        if osu_id:
-            result = await self.bot.osu.get_user(u=osu_id[0][0])
-            if result:
-                embed = await osuembed.user(result)
-                await ctx.send(result.url, embed=embed)
-            else:
-                await ctx.send(f"<https://osu.ppy.sh/users/{osu_id[0][0]}>")
+            osu_id = await cursor.fetchone()
+        if not osu_id:
+            return
+
+        result = await self.bot.osu.get_user(u=osu_id[0])
+        if not result:
+            await ctx.send(f"<https://osu.ppy.sh/users/{osu_id[0]}>")
+            return
+
+        embed = await osuembed.user(result)
+        await ctx.send(result.url, embed=embed)
 
 
 def setup(bot):
