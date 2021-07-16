5bc5211ff8bff097998f2ec4133df72cc0b35d9f
Kyuunex <kyuunex@protonmail.ch>
2021-07-17 03:03:27 +0400

    fix

--- suwako/modules/connections.py
+++ suwako/modules/connections.py
@@ -1,11 +1,11 @@
-from suwako.modules.storage_management import dirs
+from suwako.modules.storage_management import BOT_DATA_DIR
 import os
 
 if os.environ.get('SUWAKO_TOKEN'):
     bot_token = os.environ.get('SUWAKO_TOKEN')
 else:
     try:
-        with open(dirs.user_data_dir + "/token.txt", "r+") as token_file:
+        with open(BOT_DATA_DIR + "/token.txt", "r+") as token_file:
             bot_token = token_file.read().strip()
     except FileNotFoundError as e:
         print("i need a bot token. either set SUWAKO_TOKEN environment variable")
@@ -16,7 +16,7 @@ if os.environ.get('SUWAKO_OSU_API_KEY'):
     osu_api_key = os.environ.get('SUWAKO_OSU_API_KEY')
 else:
     try:
-        with open(dirs.user_data_dir + "/osu_api_key.txt", "r+") as token_file:
+        with open(BOT_DATA_DIR + "/osu_api_key.txt", "r+") as token_file:
             osu_api_key = token_file.read().strip()
     except FileNotFoundError as e:
         print("i need a osu api key. either set SUWAKO_OSU_API_KEY environment variable")
